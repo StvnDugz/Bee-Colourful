18b1a0ed83d3a76bec1cfdd70d52997997f252cd
StvnDugz <stvndugz@gmail.com>
2022-07-12 13:55:14 +0100

    Add game manager
    Added a game manager to handle when the player fails a level

---
+++ Distraction Mini Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;         // Creating an instance of the GameManger
+    public bool gameOver = false;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LevelFailed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        if (gameOver == false)
+        {
+            Debug.Log("GAME OVER");
+            gameOver = true;
+        }
+    }
+}
