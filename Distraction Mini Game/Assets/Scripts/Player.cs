9750d5c62a6b0521ded07a41c8313784d36205b6
StvnDugz <stvndugz@gmail.com>
2022-07-12 11:23:22 +0100

    Add player
    Created a player that can move on the x-axis using a rigid body, using Math.Clamp the player movement is limited to the edge of the playable area

---
+++ Distraction Mini Game/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float levelWidth = 5.5f;
+    public float levelHeight = 5f;
+    public float playerStartPosition = -3.5f;
+
+    private Rigidbody rb;
+    private float moveX;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        movement = new Vector3(moveX, 0f, 0f);
+
+        //Clamps the given value between a range defined by the given minimum and maximum values
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -levelWidth, levelWidth), Mathf.Clamp(transform.position.y, playerStartPosition, levelHeight), 0);
+
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + (movement * Time.fixedDeltaTime * speed));
+    }
+}
