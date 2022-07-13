6d9e03d9b153fdd20f9a892b146a7de41080a9ba
StvnDugz <stvndugz@gmail.com>
2022-07-13 11:56:56 +0100

    Update game manager
    Updated the game manager to implement a fade out animation when the game over state is activated

--- Distraction Mini Game/Assets/Scripts/Cube.cs
+++ Distraction Mini Game/Assets/Scripts/Cube.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public float speed = 20f;
+    private AudioSource hitAudio;       // Reference to the audio source component
     void Start()
     {
         GetComponent<Rigidbody>().drag -= Time.timeSinceLevelLoad / speed;
+        hitAudio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // If this object collides with the object tagged
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hitAudio.Play();
+        }
     }
 }
